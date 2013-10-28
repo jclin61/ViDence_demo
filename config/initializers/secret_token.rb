3b8ed761b529659278ad9472a96b122b347d319e
Jerry C. Lin <jerryclin@hotmail.com>
2013-10-28 12:58:06 -0700

    fresh file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dashcam::Application.config.secret_key_base = '0ada4804e3b18578c3ed14bc30c4652a87b03b8fb5a0e034eb7010cc48caf96e03bbb5adb8fa8097efa85be2d57aaade92d017ba82189cb6e67e18136c9a61f2'
