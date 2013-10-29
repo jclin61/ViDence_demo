4fd7b36ffb3f3d4aad05d1a166d31b5dc3a65928
Jerry C. Lin <jerryclin@hotmail.com>
2013-10-29 16:16:17 -0700

    changed admin control to users and user_controller. can now see list of all users. working on edits and delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+	def index
+		@users= User.all
+	end
+
   	def new
   		@user = User.new
   	end
@@ -11,7 +16,30 @@ class UsersController < ApplicationController
 			redirect_to new_user_path, flash: { errors: @user.errors.full_messages }
 		end
 	end
+
+	def show
+
+	end
 	
+
+  	def edit
+  	 	@user = User.find(params[:id])
+ 	end
+
+	def update
+		user = User.find(params[:id])
+	    
+			clean_params = current_user.admin? ? admin_params : user_params
+
+	 	if @user.update(clean_params)
+	 		redirect_to @user, flash: { message: "Update sucessful" }
+	 	else
+	 	 	redirect_to edit_user_path(@user), flash: { errors: @user.errors.full_messages }
+	 	end
+	 end
+
+
+
 	private
 
 	def user_params
