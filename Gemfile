b1c3cdb23fd04e63b12d825fae8d35ec8534ea5b
Jerry C. Lin <jerryclin@hotmail.com>
2013-10-30 14:13:19 -0700

    added google maps in registration, show video, show user

--- Gemfile
+++ Gemfile
@@ -37,7 +37,7 @@ gem 'coffee-rails', '~> 4.0.0'
 # gem 'therubyracer', platforms: :ruby
 
 # Use jquery as the JavaScript library
-gem 'jquery-rails'
+gem 'jquery-rails', '~>3.0.4'
 
 gem 'jquery-ui-rails'
 
---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,66 @@
+// Google Map Function using gmap3   
+$(document).ready(function(){
+        register_map()
+        video_map()
+        // note: autocomplete require on / off function so at least jQuery 1.7
+        function register_map()
+		{
+            
+            $("#reg_map").gmap3({
+				map:{
+					options:{
+						zoom:0
+				}}
+			});
+            
+            $("#address").autocomplete({
+                source: function() {
+                    $("#reg_map").gmap3({
+                        getaddress: {
+                            address: $(this).val(),
+                            callback: function(results){
+                                if (!results) return;
+                                $("#address").autocomplete("display", results, false);
+                            }
+                        }
+                    });
+                },
+                cb:{
+                    cast: function(item){
+                        return item.formatted_address;
+                    },
+                    select: function(item) {
+                        $("#reg_map").gmap3({
+                            clear: "marker",
+                            marker: {
+                                latLng: item.geometry.location
+                            },
+                            map:{
+                                options: {
+                                    center: item.geometry.location,
+                                    zoom: 10
+                                }
+                            }
+                        });
+                    }
+                }
+            })
+            .focus();
+        
+        }
+
+		function video_map(){
+
+		$(".vid_map").gmap3({
+			marker:{
+				address: $(".vid_loc").val()
+			},
+			map:{
+				options:{
+					zoom:14
+				}
+			}
+		});
+		}
+
+});
